d23897eb4d3441b35291a45736d558f80ef4868b
fusiondd94 <fusiondd94@gmail.com>
2026-07-28 02:08:46 -0400

    Phase 4: import portal-append stylesheet in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import "./design-system-append.css";
 import "./phase2-append.css";
+import "./portal-append.css";
 
 const inter = Inter({
   subsets: ["latin"],
